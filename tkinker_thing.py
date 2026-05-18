596c99df67e64ec261e7ea4e570954c89003f317
ieatricealot <panit935@gmail.com>
2026-05-18 19:00:54 -0400

    Add files via upload

---
+++ tkinker_thing.py
@@ -0,0 +1,47 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter import colorchooser
+import random
+import datetime
+
+# Create the main window
+root = Tk()
+
+# Create a frame widget
+root.geometry("300x200")
+frm = ttk.Frame(root, padding=10)
+frm.grid()
+#other stuff
+label = ttk.Label(root, font=("Arial", 18))
+
+#def commans for buttons
+def change_color():
+    cc = ['red', 'blue']
+    c = random.choice(cc)
+    root.configure(background= "#"+("%06x"%random.randint(0,16777215)))
+
+def idk():
+    e = colorchooser.askcolor(title="Choose background color")
+    root.configure(bg=e[1])
+#other def commans
+
+def update_clock():
+    current_time = datetime.datetime.now().strftime("%H:%M:%S")
+    label.config(text=f"Time: {current_time}")
+    root.after(1000, update_clock)
+
+# Create a label widget
+ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+
+# Create a button widget
+
+quit_button = ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+
+color_button = ttk.Button(frm, text="Change color", command=idk).grid(column=2, row=0)
+
+random_color_button = ttk.Button(frm, text="Randomly change color", command=change_color).grid(column=1, row=1)
+
+update_clock()
+
+# Start the main event loop
+root.mainloop()
